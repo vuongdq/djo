6ec89e1d5ad94d1608768a1f6a1b47463f222a08
vuongdq <quocvuong_it@yahoo.com>
2023-02-27 01:36:45 +0700

    Facebook & Email Tool app

--- btf/urls.py
+++ btf/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
     path('crawl/', include('crawl.urls')),
-    path('hocsinhapi/', include('hocsinh.urls')),
 ]
